04d20df32a0fa51992f7d60586789d1f64a6e1d9
Abbosjon <Abbosjondev@gmail.com>
2025-12-10 12:12:27 +0500

    Buglar tuzatildi va bazi yangi funksiyalar qo'shildi

--- shifoxona.py
+++ shifoxona.py
@@ -3,6 +3,8 @@ import time
 import json
 from datetime import datetime
 
+if not os.path.exists("ids"):
+    os.makedirs("ids")
 
 def load_json(filename):
     try:
@@ -16,6 +18,25 @@ def save_json(filename, data):
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
+
+def update_id_card(phone, event_type, details):
+    filename = f"ids/{phone}.json"
+    
+    data = load_json(filename)
+    if not data:
+        data = {
+            "phone": phone,
+            "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "history": []
+        }
+    
+    record = {
+        "event": event_type,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "details": details
+    }
+    data["history"].append(record)
+    save_json(filename, data)
 
 
 doctors_db = load_json("doctors.json")
@@ -41,29 +62,25 @@ def wait(seconds):
     time.sleep(seconds)
 
 
-
-
-
-
 def admin_panel():
     while True:
         clear()
         print_color("--- ADMIN PANEL ---", SARIQ)
-        print("1. Doktorlarni ko‘rish")
-        print("2. Doktor qo‘shish")
-        print("3. Doktor o‘chirish")
-        print("4. Foydalanuvchilar ro‘yxati")
+        print("1. Doktorlarni ko'rish")
+        print("2. Doktor qo'shish")
+        print("3. Doktor o'chirish")
+        print("4. Foydalanuvchilar ro'yxati")
         print("5. Foydalanuvchini boshqarish")
+        print("6. Foydalanuvchi ID Kartasini ko'rish")
         print("0. Chiqish")
 
         choice = input("\nTanlov: ")
 
-    
         if choice == "1":
             clear()
-            print_color("--- DOKTORLAR RO‘YXATI ---", KOK)
+            print_color("--- DOKTORLAR RO'YXATI ---", KOK)
             if not doctors_db:
-                print("Hozircha shifokorlar yo‘q.")
+                print("Hozircha shifokorlar yo'q.")
             else:
                 for k, d in doctors_db.items():
                     print(f"ID: {k} | {d['name']} | {d['type']}")
@@ -71,7 +88,7 @@ def admin_panel():
 
         elif choice == "2":
             clear()
-            print_color("--- YANGI DOKTOR QO‘SHISH ---", KOK)
+            print_color("--- YANGI DOKTOR QO'SHISH ---", KOK)
             doc_id = input("Doktor ID (login): ")
             if doc_id in doctors_db:
                 print_color("Bu ID allaqachon mavjud!", QIZIL)
@@ -86,38 +103,35 @@ def admin_panel():
                 'name': name,
                 'password': password,
                 'type': type_,
-                'bemorlar': []
+                'bemorlar': [],
+                'slots': {}
             }
 
             save_json("doctors.json", doctors_db)
-            print_color("Doktor muvaffaqiyatli qo‘shildi!", YASHIL)
+            print_color("Doktor muvaffaqiyatli qo'shildi!", YASHIL)
             wait(1.5)
 
         elif choice == "3":
             clear()
-            print_color("--- DOKTOR O‘CHIRISH ---", KOK)
-            doc_id = input("O‘chirmoqchi bo‘lgan doktor ID: ")
+            print_color("--- DOKTOR O'CHIRISH ---", KOK)
+            doc_id = input("O'chirmoqchi bo'lgan doktor ID: ")
             if doc_id not in doctors_db:
                 print_color("Bunday ID mavjud emas!", QIZIL)
             else:
                 del doctors_db[doc_id]
                 save_json("doctors.json", doctors_db)
-                print_color("Doktor o‘chirildi!", YASHIL)
+                print_color("Doktor o'chirildi!", YASHIL)
             wait(1.5)
-
-
 
         elif choice == "4":
             clear()
-            print_color("--- FOYDALANUVCHILAR RO‘YXATI ---", KOK)
-
+            print_color("--- FOYDALANUVCHILAR RO'YXATI ---", KOK)
             if not users_db:
-                print("Bazada foydalanuvchi yo‘q.")
+                print("Bazada foydalanuvchi yo'q.")
             else:
                 for phone, u in users_db.items():
                     status = "BLOKLANGAN" if u.get("blocked") else "Faol"
                     print(f"{phone} | {u['name']} | {status}")
-
             input("\nQaytish uchun Enter...")
 
         elif choice == "5":
@@ -135,7 +149,7 @@ def admin_panel():
             while True:
                 clear()
                 print_color(f"{usr['name']} ({phone})", SARIQ)
-                print("1. Kartasini ko‘rish")
+                print("1. Kartasini ko'rish")
                 print("2. Bloklash")
                 print("3. Blokdan chiqarish")
                 print("0. Chiqish")
@@ -145,13 +159,14 @@ def admin_panel():
                 if c == "1":
                     clear()
                     print_color("--- BEMOR KARTASI ---", KOK)
-
                     if not usr.get("history"):
                         print("Hali hech qaysi shifokorga murojaat qilmagan.")
                     else:
                         for i, h in enumerate(usr['history'], 1):
-                            print(f"{i}. Shifokor: {h['doctor']} ({h['type']}) | Sana: {h['time']}")
-
+                            doc_name = h.get('doctor', 'Noma\'lum') # get xatolik chiqmasligi uchun, agar [] ishlatganda hech nima bo'lmasa xatolik berardi
+                            doc_type = h.get('type', '')
+                            time_v = h.get('time', '')
+                            print(f"{i}. Shifokor: {doc_name} ({doc_type}) | Sana: {time_v}")
                     input("\nOrqaga qaytish uchun Enter...")
 
                 elif c == "2":
@@ -168,22 +183,35 @@ def admin_panel():
 
                 elif c == "0":
                     break
-
                 else:
                     print_color("Xato tanlov!", QIZIL)
                     wait(1)
-
+        
+        elif choice == "6":
+            clear()
+            print_color("--- ID CARD KO'RISH ---", KOK)
+            phone = input("Foydalanuvchi telefon raqami: ")
+            filepath = f"ids/{phone}.json"
+            
+            if os.path.exists(filepath):
+                data = load_json(filepath)
+                print_color(f"\nID Fayl: {phone}", SARIQ)
+                print(f"Yaratilgan sana: {data.get('created_at')}")
+                print("-" * 30)
+                for item in data.get('history', []):
+                    print(f"[{item['timestamp']}] -> {item['event']}")
+                    print(f"   Tafsilot: {item['details']}")
+                    print("-" * 15)
+            else:
+                print_color("Bu raqamga tegishli ID karta (fayl) topilmadi.", QIZIL)
+            
+            input("\nQaytish uchun Enter...")
 
         elif choice == "0":
             break
-
         else:
-            print_color("Noto‘g‘ri tanlov!", QIZIL)
+            print_color("Noto'g'ri tanlov!", QIZIL)
             wait(1)
-
-
-
-
 
 
 def doctor_panel(doc_username):
@@ -194,8 +222,10 @@ def doctor_panel(doc_username):
         print_color(f"--- SHIFOKOR PANELI: {current_doc['name']} ---", SARIQ)
         print(f"Mutaxassislik: {current_doc['type']}")
         print("--------------------------------")
-        print("1. Bemorlar ro‘yxati")
+        print("1. Bemorlar ro'yxati")
         print("2. Qabulni boshlash")
+        print("3. Qabul vaqtlari qo'shish")
+        print("4. Qabul vaqtlarini ko'rish")
         print("0. Chiqish")
 
         choice = input("\nTanlov: ")
@@ -204,35 +234,93 @@ def doctor_panel(doc_username):
             clear()
             print_color("--- BEMORLAR ---", KOK)
             if not current_doc['bemorlar']:
-                print("Navbatda bemor yo‘q.")
+                print("Navbatda bemor yo'q.")
             else:
                 for i, p in enumerate(current_doc['bemorlar'], 1):
-                    print(f"{i}. {p['name']} | Tel: {p['phone']} | Vaqt: {p['time']}")
+                    slot_time = p.get('selected_slot', 'Jonli navbat')
+                    print(f"{i}. {p['name']} | Tel: {p['phone']} | Vaqt: {slot_time}")
             input("\nOrqaga qaytish uchun Enter...")
 
         elif choice == "2":
             if not current_doc['bemorlar']:
-                print_color("Bemor yo‘q!", QIZIL)
+                print_color("Bemor yo'q!", QIZIL)
                 wait(1.5)
             else:
                 patient = current_doc['bemorlar'][0]
-                print(f"{patient['name']} ko‘rikka kiryapti...")
+                print(f"{patient['name']} ko'rikka kiryapti...")
                 wait(2)
+                
+                update_id_card(patient['phone'], "visit_done", {
+                    "doctor": current_doc['name'],
+                    "type": current_doc['type'],
+                    "status": "Ko'rikdan o'tdi",
+                    "slot": patient.get('selected_slot')
+                })
+                
                 current_doc['bemorlar'].pop(0)
                 save_json("doctors.json", doctors_db)
-                print_color("Ko‘rik tugadi!", YASHIL)
+                print_color("Ko'rik tugadi va tarixga yozildi!", YASHIL)
                 wait(1.5)
+
+        elif choice == "3":
+            clear()
+            print_color("--- VAQT QO'SHISH ---", KOK)
+            print("Format: 14:00-15:00")
+            new_slot = input("Vaqt oralig'ini kiriting: ")
+            
+            if "slots" not in current_doc:
+                current_doc["slots"] = {}
+                
+            if new_slot in current_doc["slots"]:
+                print_color("Bu vaqt allaqachon mavjud!", QIZIL)
+            else:
+                current_doc["slots"][new_slot] = None 
+                save_json("doctors.json", doctors_db)
+                print_color(f"{new_slot} vaqti qo'shildi!", YASHIL)
+            wait(1.5)
+
+        elif choice == "4":
+            clear()
+            print_color("--- MENING VAQTLARIM ---", KOK)
+            if "slots" not in current_doc or not current_doc["slots"]:
+                print("Vaqtlar belgilanmagan.")
+            else:
+                for time_slot, status in current_doc["slots"].items():
+                    holat = f"{QIZIL}BAND ({status}){RESET}" if status else f"{YASHIL}BO'SH{RESET}"
+                    print(f"{time_slot} - {holat}")
+            input("\nQaytish uchun Enter...")
 
         elif choice == "0":
             break
 
         else:
-            print_color("Noto‘g‘ri tanlov!", QIZIL)
+            print_color("Noto'g'ri tanlov!", QIZIL)
             wait(1)
 
 
-
-
+def show_full_id_card(phone):
+    clear()
+    filepath = f"ids/{phone}.json"
+    print_color(f"--- RAQAMLI ID KARTA: {phone} ---", KOK)
+    
+    if not os.path.exists(filepath):
+        print("ID karta fayli topilmadi.")
+    else:
+        data = load_json(filepath)
+        print(f"Ro'yxatdan o'tgan sana: {data.get('created_at', 'Noma\'lum')}")
+        print("\n--- TARIX ---")
+        for h in data.get('history', []):
+            print(f"[{h['timestamp']}] - {h['event']}")
+            details = h.get('details', {})
+            if 'doctor' in details:
+                print(f"   Doktor: {details['doctor']} ({details.get('type')})")
+            if 'slot' in details:
+                print(f"   Vaqt: {details['slot']}")
+            if 'status' in details:
+                print(f"   Status: {details['status']}")
+            print(" . . . . . . . . .")
+            
+    input("\nOrqaga qaytish uchun Enter...")
 
 
 def show_user_card(phone):
@@ -243,7 +331,7 @@ def show_user_card(phone):
         return
 
     usr = users_db[phone]
-    print_color(f"--- {usr.get('name','No-name')}NING TIBBIY KARTASI ---", KOK)
+    print_color(f"--- {usr.get('name','No-name')}NING ESKI TIBBIY KARTASI ---", KOK)
 
     if not usr.get("history"):
         print("Siz hali hech qaysi shifokorga murojaat qilmagansiz.")
@@ -256,8 +344,6 @@ def show_user_card(phone):
     input("\nOrqaga qaytish uchun Enter...")
 
 
-
-
 def user_panel(phone):
     current_user = users_db[phone]
 
@@ -266,7 +352,8 @@ def user_panel(phone):
         print_color(f"--- FOYDALANUVCHI PANELI: {current_user['name']} ---", SARIQ)
         print("1. Ma'lumotlarim")
         print("2. Shifokor qabuliga yozilish")
-        print("3. Mening kartam")
+        print("3. Mening eski kartam")
+        print("4. Mening ID Kartam")
         print("0. Chiqish")
 
         choice = input("\nTanlov: ")
@@ -283,49 +370,91 @@ def user_panel(phone):
             clear()
             print_color("--- SHIFOKORLAR ---", KOK)
 
+            doc_list = []
             for idd, d in doctors_db.items():
-                print(f"ID: {idd} | {d['name']} | {d['type']}")
+                doc_list.append((idd, d))
+                print(f"{len(doc_list)}. {d['name']} | {d['type']}")
 
             print("\n0. Orqaga")
-
-            doc_choice = input("\nShifokor ID: ")
-
-            if doc_choice == "0":
+            
+            try:
+                sel_idx = int(input("\nShifokor raqamini tanlang: ")) - 1
+            except:
+                sel_idx = -1
+            if sel_idx == -1:
                 continue
 
-            if doc_choice not in doctors_db:
-                print_color("Bunday shifokor yo‘q!", QIZIL)
+            if 0 <= sel_idx < len(doc_list):
+                selected_id, doc = doc_list[sel_idx]
+                
+                clear()
+                print_color(f"--- {doc['name']} QABUL VAQTLARI ---", SARIQ)
+                
+                available_slots = []
+                if "slots" in doc:
+                    for t, status in doc["slots"].items():
+                        if status is None:
+                            available_slots.append(t)
+                
+                if not available_slots:
+                    print_color("Bu shifokorda bo'sh vaqtlar yo'q!", QIZIL)
+                    wait(2)
+                    continue
+                
+                for i, t in enumerate(available_slots, 1):
+                    print(f"{i}. {t}")
+                
+                try:
+                    time_idx = int(input("\nVaqtni tanlang: ")) - 1
+                    if 0 <= time_idx < len(available_slots):
+                        chosen_time = available_slots[time_idx]
+                        
+                        doc['slots'][chosen_time] = phone
+                        
+                        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                        doc['bemorlar'].append({
+                            'name': current_user['name'],
+                            'phone': phone,
+                            'time': timestamp,
+                            'selected_slot': chosen_time
+                        })
+                        
+                        if "history" not in current_user:
+                            current_user["history"] = []
+                        current_user["history"].append({
+                            'doctor': doc['name'],
+                            'type': doc['type'],
+                            'time': timestamp
+                        })
+                        
+                        save_json("doctors.json", doctors_db)
+                        save_json("users.json", users_db)
+                        
+                        update_id_card(phone, "booking", {
+                            "doctor": doc['name'],
+                            "type": doc['type'],
+                            "slot": chosen_time,
+                            "status": "Band qilindi"
+                        })
+
+                        print_color(f"{chosen_time} vaqtiga yozildingiz!", YASHIL)
+                        wait(2)
+                    else:
+                        print_color("Xato vaqt tanlandi!", QIZIL)
+                        wait(1)
+                except ValueError:
+                    print_color("Raqam kiriting!", QIZIL)
+                    wait(1)
+
+            else:
+                print_color("Noto'g'ri shifokor tanlandi!", QIZIL)
                 wait(1.5)
-                continue
-
-            doc = doctors_db[doc_choice]
-
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-            doc['bemorlar'].append({
-                'name': current_user['name'],
-                'phone': phone,
-                'time': timestamp
-            })
-
-           
-            if "history" not in current_user:
-                current_user["history"] = []
-
-            current_user["history"].append({
-                'doctor': doc['name'],
-                'type': doc['type'],
-                'time': timestamp
-            })
-
-            save_json("doctors.json", doctors_db)
-            save_json("users.json", users_db)
-
-            print_color(f"{doc['name']} qabuliga yozildingiz!", YASHIL)
-            wait(2)
 
         elif choice == "3":
             show_user_card(phone)
+        
+        elif choice == "4":
+            show_full_id_card(phone)
 
         elif choice == "0":
             break
@@ -333,9 +462,6 @@ def user_panel(phone):
         else:
             print_color("Noto'g'ri tanlov!", QIZIL)
             wait(1)
-
-
-
 
 
 def login(user_type):
@@ -349,7 +475,7 @@ def login(user_type):
         if u == admin_db.get('username') and p == admin_db.get('password'):
             admin_panel()
         else:
-            print_color("Login yoki parol noto‘g‘ri!", QIZIL)
+            print_color("Login yoki parol noto'g'ri!", QIZIL)
             wait(1.5)
 
 
@@ -361,7 +487,7 @@ def login(user_type):
         if username in doctors_db and doctors_db[username]['password'] == password:
             doctor_panel(username)
         else:
-            print_color("Login yoki parol noto‘g‘ri!", QIZIL)
+            print_color("Login yoki parol noto'g'ri!", QIZIL)
             wait(1.5)
 
 
@@ -370,7 +496,7 @@ def login(user_type):
         phone = input("Telefon (+998...): ")
 
         if phone not in users_db:
-            print_color("Ro‘yxatdan o‘tish:", KOK)
+            print_color("Ro'yxatdan o'tish:", KOK)
             name = input("Ism-familiya: ")
             age = input("Yosh: ")
             password = input("Parol: ")
@@ -384,7 +510,14 @@ def login(user_type):
             }
 
             save_json("users.json", users_db)
-            print_color("Ro‘yxatdan o‘tildi!", YASHIL)
+            
+            update_id_card(phone, "register", {
+                "name": name,
+                "age": age,
+                "status": "Created"
+            })
+            
+            print_color("Ro'yxatdan o'tildi!", YASHIL)
             wait(1.5)
             user_panel(phone)
 
@@ -399,7 +532,7 @@ def login(user_type):
             if password == users_db[phone]["password"]:
                 user_panel(phone)
             else:
-                print_color("Parol noto‘g‘ri!", QIZIL)
+                print_color("Parol noto'g'ri!", QIZIL)
                 wait(2)
 
 
@@ -420,8 +553,8 @@ while True:
     elif c == "3":
         login("user")
     elif c == "0":
-        print_color("Dastur to‘xtatildi.", YASHIL)
+        print_color("Dastur to'xtatildi.", YASHIL)
         break
     else:
-        print_color("Noto‘g‘ri tanlov!", QIZIL)
+        print_color("Noto'g'ri tanlov!", QIZIL)
         wait(1)
